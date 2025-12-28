08e9cdc6b96dd422bc3196b76da66bcca9a217b4
kooooohe <1211lta+github@gmail.com>
2025-12-28 17:40:19 +0900

    Add Admin and Tenant layout components for routing structure

---
+++ app/routes/admn.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router";
+
+export default function AdminLayout() {
+  return (
+    <div className="flex h-screen bg-gray-50">
+      {/* サイドバーナビゲーション */}
+      <div className="w-64 bg-white shadow-lg">
+      </div>
+      
+      {/* メインコンテンツエリア */}
+      <div className="flex-1 overflow-auto">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+
